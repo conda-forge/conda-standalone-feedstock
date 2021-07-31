0b5f54f1c352d2a25d8d61c4c4f3291e78f7518b
Eric Prestat <eric.prestat@gmail.com>
2021-07-31 19:13:31 +0100

    Pass prefix to conda_standalone to make sure the menus are removed when uninstalling

--- recipe/entry_point.py
+++ recipe/entry_point.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
            if args.make_menus is not None:
                module.mk_menus(remove=False, prefix=args.prefix, pkg_names=args.make_menus, root_prefix=args.prefix)
            else:
-               module.rm_menus()
+               module.rm_menus(prefix=args.prefix, root_prefix=args.prefix)
        sys.exit()
     else:
        from conda.cli import main
