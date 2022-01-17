f317ec9f150e51249172bbd2d7c7d41703229aee
Eric Prestat <eric.prestat@gmail.com>
2022-01-17 12:15:42 +0000

    Use menuinst 1.4.18 and remove corresponding patches (#24)
    * Use menuinst 1.4.18 and constructor 3.2.2 to simplify recipe and remove patches
    
    * MNT: Re-rendered with conda-build 3.21.7, conda-smithy 3.16.1, and conda-forge-pinning 2022.01.04.17.59.36

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,6 @@ for fname in "core/path_actions.py" "utils.py" "core/portability.py" "gateways/d
   cp conda_src/conda/${fname} $SP_DIR/conda/${fname}
 done
 
-# # patched menuinst files - windows only, so ignore these
-# cp menuinst_src/menuinst/__init__.py ${SP_DIR}/menuinst/__init__.py
-# cp menuinst_src/menuinst/win32.py ${SP_DIR}/menuinst/win32.py
-
 # -F is to create a single file
 # -s strips executables and libraries
 pyinstaller conda.exe.spec
