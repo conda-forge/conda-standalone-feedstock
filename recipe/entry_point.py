db9b06e5438033e0608976a74dbfe0ff3ed0e38b
Faustin Carter <fwcarter@hrl.com>
2022-12-02 14:01:51 -0800

    Include tqdm in build spec; refactor argparse custom action for better error messages

--- recipe/entry_point.py
+++ recipe/entry_point.py
@@ -20,31 +20,6 @@ CPU_COUNT = os.cpu_count()
 # https://dholth.github.io/conda-benchmarks/#extract.TimeExtract.time_extract?conda-package-handling=2.0.0a2&p-format='.conda'&p-format='.tar.bz2'&p-lang='py'&x-axis=format
 DEFAULT_NUM_WORKERS = 1 if not CPU_COUNT else min(3, CPU_COUNT)
 
-def validate_max_workers(num_str: str) -> int:
-    """Converts a string representing the max number of workers to an integer
-    while performing validation checks; raises ValueError if anything fails."""
-
-    ERROR_MSG = "Max workers must be integer between 1 and the CPU count of the machine."
-
-    try:
-        num = int(num_str)
-    except ValueError as exc:
-        raise ValueError(ERROR_MSG) from exc
-
-    if (num < 1):
-        raise ValueError(ERROR_MSG)
-
-    # cpu_count can return None, so skip this check if that happens
-    if CPU_COUNT:
-
-        # See Windows notes for magic number of 61
-        # https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor
-        max_cpu_num = min(CPU_COUNT, 61) if (os.name == "nt") else CPU_COUNT
-
-        if (num > max_cpu_num):
-            raise ValueError(ERROR_MSG)
-
-    return num
 
 
 if __name__ == '__main__':
@@ -58,76 +33,105 @@ if __name__ == '__main__':
         del sys.path[0]
 
     if len(sys.argv) > 1 and sys.argv[1].startswith('constructor'):
-       import os
-       import argparse
-       from conda.base.constants import CONDA_PACKAGE_EXTENSIONS
-       p = argparse.ArgumentParser(description="constructor args")
-       p.add_argument(
-               '--prefix',
-               action="store",
-               required="True",
-               help="path to conda prefix")
-       p.add_argument(
-               '--extract-conda-pkgs',
-               action="store_true",
-               help="extract all packages in $PREFIX/pkgs where $PREFIX is set by --prefix")
-       p.add_argument(
-               '--num-processors',
-               action="store",
-               default=DEFAULT_NUM_WORKERS,
-               type=validate_max_workers,
-               help="The number of processors to use for parallel package extraction")
-       p.add_argument(
-               '--extract-tarball',
-               action="store_true",
-               help="extract tarball from stdin")
-       p.add_argument(
-               '--make-menus',
-               nargs='*',
-               metavar='MENU_PKG',
-               help="make menus")
-       p.add_argument(
-               '--rm-menus',
-               action="store_true",
-               help="rm menus")
-       args, args_unknown = p.parse_known_args()
-       args.prefix = os.path.abspath(args.prefix)
-       os.chdir(args.prefix)
-       if args.extract_conda_pkgs:
-           import tqdm
-           from conda_package_handling import api
-           from concurrent.futures import ProcessPoolExecutor
+        import os
+        import argparse
+        from conda.base.constants import CONDA_PACKAGE_EXTENSIONS
 
-           executor = ProcessPoolExecutor(max_workers=args.max_workers)
 
-           os.chdir("pkgs")
-           flist = []
-           for ext in CONDA_PACKAGE_EXTENSIONS:
-               for pkg in os.listdir(os.getcwd()):
-                   if pkg.endswith(ext):
-                       fn = os.path.join(os.getcwd(), pkg)
-                       flist.append(fn)
-           with tqdm.tqdm(total=len(flist), leave=False) as t:
-               for fn, _ in zip(flist, executor.map(api.extract, flist)):
-                   t.set_description("Extracting : %s" % os.path.basename(fn))
-                   t.update()
+        class NumProcessorsAction(argparse.Action):
+            def __call__(self, parser, namespace, values, option_string=None):
+                """Converts a string representing the max number of workers to an integer
+                while performing validation checks; raises argparse.ArgumentError if anything fails."""
 
-       if args.extract_tarball:
-           import tarfile
-           t = tarfile.open(mode="r|*", fileobj=sys.stdin.buffer)
-           t.extractall()
-           t.close()
-       if (args.make_menus is not None) or args.rm_menus:
-           import importlib.util
-           utility_script = os.path.join(args.prefix, "Lib", "_nsis.py")
-           spec = importlib.util.spec_from_file_location("constructor_utils", utility_script)
-           module = importlib.util.module_from_spec(spec)
-           spec.loader.exec_module(module)
-           if args.make_menus is not None:
-               module.mk_menus(remove=False, prefix=args.prefix, pkg_names=args.make_menus, root_prefix=args.prefix)
-           else:
-               module.rm_menus(prefix=args.prefix, root_prefix=args.prefix)
-       sys.exit()
+                ERROR_MSG = "Max workers must be integer between 1 and the CPU count of the machine."
+
+                try:
+                    num = int(values)
+                except ValueError as exc:
+                    raise argparse.ArgumentError(self, ERROR_MSG) from exc
+
+                if (num < 1):
+                    raise argparse.ArgumentError(self, ERROR_MSG)
+
+                # cpu_count can return None, so skip this check if that happens
+                if CPU_COUNT:
+
+                    # See Windows notes for magic number of 61
+                    # https://docs.python.org/3/library/concurrent.futures.html#processpoolexecutor
+                    max_cpu_num = min(CPU_COUNT, 61) if (os.name == "nt") else CPU_COUNT
+
+                    if (num > max_cpu_num):
+                        raise argparse.ArgumentError(self, ERROR_MSG)
+
+                setattr(namespace, self.dest, num)
+
+
+        p = argparse.ArgumentParser(description="constructor args")
+        p.add_argument(
+                '--prefix',
+                action="store",
+                required="True",
+                help="path to conda prefix")
+        p.add_argument(
+                '--extract-conda-pkgs',
+                action="store_true",
+                help="extract all packages in $PREFIX/pkgs where $PREFIX is set by --prefix")
+        p.add_argument(
+                '--num-processors',
+                action=NumProcessorsAction,
+                default=DEFAULT_NUM_WORKERS,
+                help="The number of processors to use for parallel package extraction")
+        p.add_argument(
+                '--extract-tarball',
+                action="store_true",
+                help="extract tarball from stdin")
+        p.add_argument(
+                '--make-menus',
+                nargs='*',
+                metavar='MENU_PKG',
+                help="make menus")
+        p.add_argument(
+                '--rm-menus',
+                action="store_true",
+                help="rm menus")
+        args, args_unknown = p.parse_known_args()
+        args.prefix = os.path.abspath(args.prefix)
+        os.chdir(args.prefix)
+        if args.extract_conda_pkgs:
+            import tqdm
+            from conda_package_handling import api
+            from concurrent.futures import ProcessPoolExecutor
+
+            executor = ProcessPoolExecutor(max_workers=args.num_processors)
+
+            os.chdir("pkgs")
+            flist = []
+            for ext in CONDA_PACKAGE_EXTENSIONS:
+                for pkg in os.listdir(os.getcwd()):
+                    if pkg.endswith(ext):
+                        fn = os.path.join(os.getcwd(), pkg)
+                        flist.append(fn)
+            with tqdm.tqdm(total=len(flist), leave=False) as t:
+                for fn, _ in zip(flist, executor.map(api.extract, flist)):
+                    t.set_description("Extracting : %s" % os.path.basename(fn))
+                    t.update()
+
+        if args.extract_tarball:
+            import tarfile
+            t = tarfile.open(mode="r|*", fileobj=sys.stdin.buffer)
+            t.extractall()
+            t.close()
+        if (args.make_menus is not None) or args.rm_menus:
+            import importlib.util
+            utility_script = os.path.join(args.prefix, "Lib", "_nsis.py")
+            spec = importlib.util.spec_from_file_location("constructor_utils", utility_script)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            if args.make_menus is not None:
+                module.mk_menus(remove=False, prefix=args.prefix, pkg_names=args.make_menus, root_prefix=args.prefix)
+            else:
+                module.rm_menus(prefix=args.prefix, root_prefix=args.prefix)
+        sys.exit()
     else:
-       from conda.cli import main
-       sys.exit(main())
+        from conda.cli import main
+        sys.exit(main())
