b775b8d5e25aeb83a99a7747ae1007f2d77d96ef
jaimergp <jaimergp@users.noreply.github.com>
2023-08-28 08:42:24 +0200

    Update scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,35 @@
-COPY conda_src\conda\core\path_actions.py "%SP_DIR%\conda\core\path_actions.py"
-COPY conda_src\conda\utils.py "%SP_DIR%\conda\utils.py"
+@ECHO on
+
+RENAME "%SP_DIR%\conda\core\path_actions.py" path_actions.py.bak || goto :error
+COPY conda_src\conda\core\path_actions.py "%SP_DIR%\conda\core\path_actions.py" || goto :error
+RENAME "%SP_DIR%\conda\utils.py" utils.py.bak || goto :error
+COPY conda_src\conda\utils.py "%SP_DIR%\conda\utils.py" || goto :error
+RENAME "%SP_DIR%\conda\deprecations.py" deprecations.py.bak || goto :error
+COPY conda_src\conda\deprecations.py "%SP_DIR%\conda\deprecations.py" || goto :error
 
 :: we need these for noarch packages with entry points to work on windows
-COPY conda_src\conda\shell\cli-%ARCH%.exe entry_point_base.exe
+COPY "conda_src\conda\shell\cli-%ARCH%.exe" entry_point_base.exe || goto :error
 
 :: This is ordinarily installed by the installer itself, but since we are building for a
 :: standalone and have only an env, not an installation, include it here.
-COPY constructor\constructor\nsis\_nsis.py "%PREFIX%\Lib\_nsis.py"
+COPY constructor_src\constructor\nsis\_nsis.py "%PREFIX%\Lib\_nsis.py" || goto :error
 
-pyinstaller conda.exe.spec
-MKDIR "%PREFIX%\standalone_conda"
-MOVE dist\conda.exe "%PREFIX%\standalone_conda\conda.exe"
+pyinstaller --clean --log-level=DEBUG src\conda.exe.spec || goto :error
+MKDIR "%PREFIX%\standalone_conda" || goto :error
+MOVE dist\conda.exe "%PREFIX%\standalone_conda\conda.exe" || goto :error
 
-RD /s /q "%PREFIX%\lib"
+:: Collect licenses
+%PYTHON% src\licenses.py ^
+  --prefix "%BUILD_PREFIX%" ^
+  --include-text ^
+  --text-errors replace ^
+  --output "%SRC_DIR%\3rd-party-licenses.json" || goto :error
+
+RD /s /q "%PREFIX%\lib" || goto :error
+
+goto :EOF
+
+:error
+set "exitcode=%errorlevel%"
+echo Failed with error #%exitcode%.
+exit /b %exitcode%
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,20 +2,30 @@ set -euxo pipefail
 
 # patched conda files
 # new files in patches need to be added here
-for fname in "core/path_actions.py" "utils.py"; do
-  cp conda_src/conda/${fname} $SP_DIR/conda/${fname}
+for fname in "core/path_actions.py" "utils.py" "deprecations.py"; do
+  mv "$SP_DIR/conda/${fname}" "$SP_DIR/conda/${fname}.bak"
+  cp "conda_src/conda/${fname}" "$SP_DIR/conda/${fname}"
 done
 
-# `base` conda might use sigtool, which ships a codesign binary that shadows Apple's one
-# pyinstaller expects that one first in PATH
-if [[ $target_platform = "osx-"* ]]; then
+# make sure pyinstaller finds Apple's codesign first in PATH
+# some base installations have 'sigtool', which ships a
+# 'codesign' binary that might shadow Apple's codesign
+if [[ $target_platform == osx-* ]]; then
   ln -s /usr/bin/codesign "$BUILD_PREFIX/bin/codesign"
 fi
 
 # -F is to create a single file
 # -s strips executables and libraries
-pyinstaller conda.exe.spec
-mkdir -p $PREFIX/standalone_conda
-mv dist/conda.exe $PREFIX/standalone_conda
+pyinstaller --clean --log-level=DEBUG src/conda.exe.spec
+mkdir -p "$PREFIX/standalone_conda"
+mv dist/conda.exe "$PREFIX/standalone_conda"
+
+# Collect licenses
+python src/licenses.py \
+  --prefix "$BUILD_PREFIX" \
+  --include-text \
+  --text-errors replace \
+  --output "$SRC_DIR/3rd-party-licenses.json"
+
 # clean up .pyc files that pyinstaller creates
-rm -rf $PREFIX/lib
+rm -rf "$PREFIX/lib"
