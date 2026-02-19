2edfc653f8b9caeaf9eece163368813815415fab
jaimergp <jaimergp@users.noreply.github.com>
2026-02-19 13:03:03 +0100

    Update build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,9 @@
 @ECHO on
 
-RENAME "%SP_DIR%\conda\core\path_actions.py" path_actions.py.bak || goto :error
-COPY conda_src\conda\core\path_actions.py "%SP_DIR%\conda\core\path_actions.py" || goto :error
-RENAME "%SP_DIR%\conda\utils.py" utils.py.bak || goto :error
-COPY conda_src\conda\utils.py "%SP_DIR%\conda\utils.py" || goto :error
-RENAME "%SP_DIR%\conda\deprecations.py" deprecations.py.bak || goto :error
-COPY conda_src\conda\deprecations.py "%SP_DIR%\conda\deprecations.py" || goto :error
-RENAME "%SP_DIR%\conda\base\constants.py" constants.py.bak || goto :error
-COPY conda_src\conda\base\constants.py "%SP_DIR%\conda\base\constants.py" || goto :error
+"%PYTHON%" "%SRC_DIR%\recipe\copy_patches.py" ^
+  --patch-source "%SRC_DIR%\src\conda_patches" ^
+  --site-packages "%SP_DIR%" ^
+  --conda-source conda_src || goto :error
 
 :: we need these for noarch packages with entry points to work on windows
 COPY "conda_src\conda\shell\cli-%ARCH%.exe" entry_point_base.exe || goto :error
